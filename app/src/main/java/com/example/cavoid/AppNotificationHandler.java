6200bcf508996d742f1d1dead6669bef13dd867d
William Coleman <will.colmn@gmail.com>
2020-09-30 14:12:08 -0400

    Revert break2

--- app/src/main/java/com/example/cavoid/AppNotificationHandler.java
+++ app/src/main/java/com/example/cavoid/AppNotificationHandler.java
@@ -16,7 +16,8 @@ public class AppNotificationHandler {
 
     public static void deliverNotification(Context context, String title, String message){
         Toast.makeText(context,"hello",Toast.LENGTH_SHORT).show();
-        NotificationManager mNotificationManager = (NotificationManager) context.getSystemService(Context.NOTIF_
+        NotificationManager mNotificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+
         NotificationCompat.Builder builder = new NotificationCompat.Builder(context,PRIMARY_CHANNEL_ID)
                 .setSmallIcon(R.drawable.ic_trend_up)
                 .setContentTitle(title)
