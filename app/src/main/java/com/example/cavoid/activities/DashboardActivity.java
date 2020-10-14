1f067048a37ad01895921c64e611195196c91bc4
Starstrum <dudleymc@vcu.edu>
2020-10-14 03:21:46 -0400

    -Names of textviews and strings make sense -Updated case and death messages to show more statistics

--- app/src/main/java/com/example/cavoid/activities/DashboardActivity.java
+++ app/src/main/java/com/example/cavoid/activities/DashboardActivity.java
@@ -17,6 +17,8 @@ import android.widget.TextView;
 import com.android.volley.Response;
 import com.example.cavoid.R;
 import com.example.cavoid.api.Repository;
+import com.example.cavoid.database.LocationDao;
+import com.example.cavoid.database.LocationDatabase;
 import com.example.cavoid.workers.RegularLocationSaveWorker;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
@@ -32,13 +34,11 @@ import java.util.Calendar;
 import java.util.Date;
 
 public class DashboardActivity extends AppCompatActivity {
-    //String countyName = "";
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
-        getCountyName();
+        updateDashBoard();
     }
     public Date yesterday() {
         final Calendar cal = Calendar.getInstance();
@@ -50,13 +50,16 @@ public class DashboardActivity extends AppCompatActivity {
         return dateFormat.format(yesterday());
     }
 
-
-    public ListenableWorker.Result getCountyName(){
+    public ListenableWorker.Result updateDashBoard(){
         TextView currentCounty = (TextView) findViewById(R.id.greetingTextView);
-        TextView countyCaseNumber = (TextView) findViewById(R.id.casesTextView);
-        TextView countyDeathNumber = (TextView) findViewById(R.id.deathsTextView);
+        TextView newCases = (TextView) findViewById(R.id.casesTextView);
+        TextView newDeaths = (TextView) findViewById(R.id.deathsTextView);
+        TextView pastLocationCases =(TextView) findViewById(R.id.pastCasesTextView);
+        TextView pastLocationDeaths = (TextView) findViewById(R.id.pastDeathsTextView);
+
         FusedLocationProviderClient fusedLocationClient = LocationServices.getFusedLocationProviderClient(getApplicationContext());
         String TAG = RegularLocationSaveWorker.class.getName();
+
         if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             return ListenableWorker.Result.failure();
         }
@@ -82,29 +85,37 @@ public class DashboardActivity extends AppCompatActivity {
 
                                 currentCounty.setText("Here are the covid statistics for " + countyName);
 
+                                //LocationDao locDao = LocationDatabase.getLocationDao();
+
+                                //This gets the statistics for the current county and sets the first card
                                 Repository.getPosTests(getApplicationContext(), countyfips, new Response.Listener<JSONObject>() {
                                     @Override
                                     public void onResponse(JSONObject response) {
                                         try {
-
-                                            String caseNumber = response.getString("cases");
-                                            String reportDate = response.getString("report_date");
-                                            String deathNumber = response.getString("deaths");
-
-                                            String caseMessage = "Cases on " + yesterday + ": " + caseNumber;
-                                            String deathMessage = "Deaths on " + yesterday + ": " + deathNumber;
-                                            countyCaseNumber.setText(caseMessage);
-                                            countyDeathNumber.setText(deathMessage);
+                                            //String reportDate = response.getString("report_date");
+                                            String newCaseNumber = response.getString("new_daily_cases");
+                                            String newDeathNumber = response.getString("new_daily_deaths");
+                                            String activeCases = response.getString("active_cases_est");
+                                            String totalCases = response.getString("cases");
+                                            String totalDeaths = response.getString("deaths");
+                                            String caseMessage = "New cases on " + yesterday + ": " + newCaseNumber
+                                                    + " Active Cases: " + activeCases + " Total Cases: " + totalCases;
+                                            String deathMessage = "New deaths on " + yesterday + ": " + newDeathNumber
+                                                    + " Total Cases: " + totalDeaths;
+                                            newCases.setText(caseMessage);
+                                            newDeaths.setText(deathMessage);
                                         } catch (JSONException e) {
-                                            countyCaseNumber.setText("beep");
                                         }
-
                                     }
                                 });
 
+                                //Go through the database of past locations then for each fips get statistics for that county
+                                //update pastLocationCases and pastLocationDeaths with the current text + next past location stats
+
+
                             }
                             catch(JSONException e) {
-                                countyCaseNumber.setText("poop");
+
                             }
 
                         }
